1181e5ae35b61592e372fc80efe7df6d8e502203
gakki <857119664@qq.com>
2026-05-28 20:33:53 +0800

    u

--- src/modules/admin-package/admin-package.module.ts
+++ src/modules/admin-package/admin-package.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../database/database.module';
 import { PackageModule } from '../package/package.module';
 import { AdminPackageController } from './admin-package.controller';
 
 @Module({
-	imports: [PackageModule],
+	imports: [DatabaseModule, PackageModule],
 	controllers: [AdminPackageController],
 })
 export class AdminPackageModule {}
